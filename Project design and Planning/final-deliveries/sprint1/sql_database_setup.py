feae65e69dec37b144af307878c705cc8dd435b8
Janarthanan K <113234638+janarth73@users.noreply.github.com>
2022-11-19 14:47:23 +0530

    Delete Project design and Planning/final-deliveries directory

--- Project design and Planning/final-deliveries/sprint1/sql_database_setup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('costomerdatabase.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE students (name TEXT,phone TEXT,email TEXT, password text,addr TEXT, city TEXT, pin TEXT)')
-print("Table created successfully")
-conn.close()
